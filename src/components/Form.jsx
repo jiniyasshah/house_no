0dc1752b5d3ed3d3345cd6e7d9ef7317629da0e3
Jiniyas Shah <72019447+jiniyasshah@users.noreply.github.com>
2024-01-24 21:35:12 +0545

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -58,7 +58,7 @@ const Form = () => {
             try {
 
 
-                const response = await fetch("http://localhost:5000/api/submitData", {
+                const response = await fetch("https://housenumber.onrender.com/api/submitData", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
